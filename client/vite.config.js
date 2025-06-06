ba3a7d9e6a45f60c298e5d3a332078175998ec1f
shraddha-dotcom <shraddhat446@gmail.com>
2025-06-06 19:36:01 +0530

    changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: './',                    //  relative path for Netlify if deploying from `dist/`
+    base: '/',                    //  relative path for Netlify if deploying from `dist/`
   plugins: [react()],
    test: {
     globals: true,
