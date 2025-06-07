6a386c445175b31daf96001fee95d869e75e8b63
shraddha-dotcom <shraddhat446@gmail.com>
2025-06-07 19:13:44 +0530

    changes

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:5000/api";;
+export const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:5000/api";
 console.log("Base URL is:", import.meta.env.VITE_BASE_URL)
